5674d72ce124cb5f500c401600bebd530891adce
Painter21 <paulpainter21@gmail.com>
2023-10-30 14:12:31 +0100

    Some changes, still wont work

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import time
 arduino = serial.Serial(port='COM3', baudrate=115200, timeout=.1)
 
 
-def write_read(x):
-    arduino.write(x.encode())
-    print(x.encode())
+def write_read(input_command):
+    input_command.strip()  # Strip all EOL characters for consistency
+    print('Sending: ' + input_command),
+    input_command = (bytes(input_command + '\n', 'utf-8'))
+    arduino.write(input_command)  # Send g-code block to grbl
+    answer = arduino.readline().decode()  # Wait for grbl response with carriage return
+    print(' : ' + answer)
     time.sleep(0.05)
-    data = arduino.readline()
-    return data
 
 
 def main():
@@ -17,8 +19,7 @@ def main():
         input_command = input("Enter a command: ")  # Taking input from user
         if input_command == "done":
             break
-        value = write_read(input_command)
-        print(value)  # printing the value
+        write_read(input_command)
     arduino.close()
 
 
